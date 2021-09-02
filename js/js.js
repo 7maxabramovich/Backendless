632a50e07cfd0b4ba021f0f8b105911c01a41ee5
7maxabramovich <7maxabramovich@gmail.com>
2021-09-02 21:24:33 +0300

    all

--- js/js.js
+++ js/js.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
 
   $('input').click(function() {
-    $('input').removeAttr('checked');
-    $(this).attr('checked', 'true');
-    $('[id="' + $(this).attr('id') + '"').attr('checked', 'true');
     $('body').attr('class', $(this).attr('id'));
   });
-
-  $('label').click(function() {
-    let text = $(this).text();
-    $('.drop-wrap input').val(text);
+  
+  $('button').click(function(event) {
+    $('button').removeAttr('checked');
+    $(this).attr('checked', 'true');
+    $('[value="' + $(this).attr('value') + '"').attr('checked', 'true');
+    $('body').attr('class', `${(event.target.value)}`);
   });
   
-  $('.drop-wrap').click(function() {
-    $('.drop-wrap').next('.drop-wrap-content').slideToggle();
-    if ($('.drop-wrap').attr('data-active')) {
-      $('.drop-wrap').removeAttr('data-active');
-    } else {
-      $('.drop-wrap').attr('data-active', 'active');
-    }
+  $('select').change(function(event) {
+    $('body').attr('class', `${(event.target.value)}`);
   });
 
 });
